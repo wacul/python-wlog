42ed6f6f2cfbee44f35ed9a7b8938eedc40feaa5
podhmo <ababjam61+github@gmail.com>
2021-06-22 15:17:20 +0900

    fix: for python -m pydoc wlog

--- wlog/__init__.py
+++ wlog/__init__.py
@@ -1,6 +1,6 @@
 from wlog._logger import get_logger
-from wlog._logger import setup  # noqa: F401
-from wlog._logger import suppress_logging  # noqa: F401
+from wlog._logger import setup
+from wlog._logger import suppress_logging
 
 getLogger = get_logger  # backward comapatibility of python stdlib
 
@@ -8,3 +8,5 @@ getLogger = get_logger  # backward comapatibility of python stdlib
 # AIA_LOG_FORMART=text python <file.py>
 # AIA_LOG_LEVEL=debug AIA_LOG_FORMAT=text python <file.py>
 # python <file.py>
+
+__all__ = ["setup", "get_logger", "suppress_logging"]
